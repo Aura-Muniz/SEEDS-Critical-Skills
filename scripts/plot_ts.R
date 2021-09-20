6d35bf79efb3832fbc6455bb759f19be83185667
Aura Muniz <aura.muniztorres001@umb.edu>
2021-09-20 17:48:28 -0400

    Improve comments in script

--- scripts/plot_ts.R
+++ scripts/plot_ts.R
@@ -22,3 +22,33 @@ fig1<-ggplot()+
   geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity",width=5)
 fig1
 
+
+########Comparison with Jessica's and kristina's script
+
+# libraries
+library(dplyr)
+library(ggplot2)
+
+
+#data manual
+d<-read.csv("wp.csv")
+str(d)
+
+#data automated
+new_d1 <- load("../psychrometer_append/clean3/new_1b_3.r")
+str(new_d1)
+
+#function
+SE<-function(x){sd(x,na.rm=T)/sqrt(length(x))}
+sum_WP<-d%>%
+  mutate(datetime=as.POSIXct(strptime(datetime, "%m/%d/%Y %H:%M")),
+         date=as.POSIXct(paste0(date," 00:00"), format="%m/%d/%Y %H:%M"))%>%
+  group_by(date)%>%
+  summarize(m=mean(negWP,na.rm=T),se=SE(negWP),sd=sd(negWP))
+
+#figure
+fig1<-ggplot()+
+  geom_point(data=new_d1, aes(x=datetime, y=psy))+
+  geom_point(data=sum_WP, aes(x=date, y=m), stat="identity", size=3, col="red")+
+  geom_errorbar(data=sum_WP, aes(x=date, ymin=m-sd, ymax=m+sd), stat="identity",width=5)
+fig1
